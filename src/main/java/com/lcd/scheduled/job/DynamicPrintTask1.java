ce05e68c611809cefe0a224f32e78022c5f2b3e3
linchengdian <573232454@qq.com>
2020-11-09 10:39:01 +0800

    使用@RefreshScope注解实现本地配置文件动态刷新

--- src/main/java/com/lcd/scheduled/job/DynamicPrintTask1.java
+++ src/main/java/com/lcd/scheduled/job/DynamicPrintTask1.java
@@ -2,15 +2,20 @@ package com.lcd.scheduled.job;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 public class DynamicPrintTask1 implements ScheduledOfTask {
+    @Value("${task.output}")
+    private String output;
     private Logger logger = LoggerFactory.getLogger(getClass());
     private int i;
     @Override
     public void execute() {
-        logger.info("thread id:{},DynamicPrintTask1 execute times:{}", Thread.currentThread().getId(), ++i);
+        logger.info("thread id:{},DynamicPrintTask1 execute times:{},OutPut:{}", Thread.currentThread().getId(), ++i,output);
     }
 
 }
