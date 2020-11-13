cd90228228d7930579e41ee9a9677d99cf550cd4
linchengdian <573232454@qq.com>
2020-11-13 11:24:15 +0800

    使用@Slf4j注解

--- src/main/java/com/lcd/scheduled/job/DynamicPrintTask.java
+++ src/main/java/com/lcd/scheduled/job/DynamicPrintTask.java
@@ -1,16 +1,17 @@
 package com.lcd.scheduled.job;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class DynamicPrintTask implements ScheduledOfTask {
-    private Logger logger = LoggerFactory.getLogger(getClass());
     private int i;
     @Override
     public void execute() {
-        logger.info("thread id:{},DynamicPrintTask execute times:{}", Thread.currentThread().getId(), ++i);
+        log.info("thread id:{},DynamicPrintTask execute times:{}", Thread.currentThread().getId(), ++i);
     }
 
 }
--- src/main/java/com/lcd/scheduled/job/DynamicPrintTask1.java
+++ src/main/java/com/lcd/scheduled/job/DynamicPrintTask1.java
@@ -1,5 +1,6 @@
 package com.lcd.scheduled.job;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,14 +9,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RefreshScope
+@Slf4j
 public class DynamicPrintTask1 implements ScheduledOfTask {
     @Value("${task.output}")
     private String output;
-    private Logger logger = LoggerFactory.getLogger(getClass());
     private int i;
     @Override
     public void execute() {
-        logger.info("thread id:{},DynamicPrintTask1 execute times:{},OutPut:{}", Thread.currentThread().getId(), ++i,output);
+        log.info("thread id:{},DynamicPrintTask1 execute times:{},OutPut:{}", Thread.currentThread().getId(), ++i,output);
     }
 
 }
